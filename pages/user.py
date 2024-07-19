3e46522bd0e9fd40ba24384f99b7c459b9581601
EC2 Default User <ec2-user@ip-172-31-31-209.ap-northeast-1.compute.internal>
2024-07-19 01:54:06 +0000

    multi page

---
+++ pages/user.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from menu import menu_with_redirect
+
+# Redirect to app.py if not logged in, otherwise show the navigation menu
+menu_with_redirect()
+
+st.title("This page is available to all users")
+st.markdown(f"You are currently logged with the role of {st.session_state.role}.")
